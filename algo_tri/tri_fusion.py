ea88f7624dbe81e3a5adea901e6cfff0c45cd119
UBONGO2000 <gadyubongo@gmail.com>
2024-09-25 17:43:52 +0200

    exos debut non fonctionnelle du trie de fusion : juste la fonction fusionner

--- algo_tri/tri_fusion.py
+++ algo_tri/tri_fusion.py
@@ -1,10 +1,21 @@
-tableau_tri=[]
-def fusionner(tab1, tab2) :
-    for i in range(len(tab1)) :
-        if(tab1[i] < tab2[i]):
-            tableau_tri.append(tab2[i])
-        else:
-            if(tab1[i]>tab2[i+1]):
-                tableau_tri.append(tab2[i])
+T=[]
+def fusinner(tab1,tab2):
+        for i  in range(len(tab1)):
+                j=i
+                while(j<len(tab2)):
+                        if(tab1[i]>tab2[j]):
+                                T.append(tab1[i])
+                                j=len(tab2)+1
+                        else:
+                                T.append(tab2[j])
+                                j+=1
 
 
+
+tab1=[8,27,33]
+tab2=[14,16,31,54]
+
+fusinner(tab1,tab2)
+
+
+print(T)
