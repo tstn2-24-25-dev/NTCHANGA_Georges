a32431b4af43d0139842aed9cbdf2351722912c5
UBONGO2000 <gadyubongo@gmail.com>
2024-09-26 07:10:45 +0200

    fin de la fonction fusionner manque fonction triinsertion

--- algo_tri/tri_fusion.py
+++ algo_tri/tri_fusion.py
@@ -1,15 +1,23 @@
 T=[]
 def fusinner(tab1,tab2):
         for i  in range(len(tab1)):
+                max_tab1= max(tab1)
+                max_tab2= max(tab2)
+                if(max_tab1>max_tab2):
+                        a=tab1.index(max_tab1)
+                else:
+                        a=tab2.index(max_tab2)
                 j=i
                 while(j<len(tab2)):
-                        if(tab1[i]>tab2[j]):
+                        if(tab1[i]<tab2[j]):
                                 T.append(tab1[i])
                                 j=len(tab2)+1
                         else:
                                 T.append(tab2[j])
+                                a=j
                                 j+=1
-
+                #T.extend(tab1[i:])
+        T.extend(tab2[a:])
 
 
 tab1=[8,27,33]
